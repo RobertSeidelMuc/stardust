2997f490e416b4aac02b8e28b9c49d1f9999a5ea
Robert Seidel <robertseidel@mail.de>
2020-10-27 11:21:25 +0100

    Randomized star position

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -6,6 +6,7 @@ export default function Game() {
   const [screenHeight, setScreenHeight] = useState("0");
   const [gridWidth, setGridWidth] = useState("0");
   const [gridHeight, setGridHeight] = useState("0");
+  const [stars, setStars] = useState([]);
   const [starDiameter, setStarDiameter] = useState("0");
   const starSizeFactor = 14;
   const grid = { columns: 8, rows: 4 };
@@ -17,6 +18,27 @@ export default function Game() {
       window.removeEventListener("resize", handleResize);
     };
   });
+
+  useEffect(() => {
+    function randomize(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    const newStars = [];
+    for (let r = 0; r < grid.rows; r++) {
+      for (let c = 0; c < grid.columns; c++) {
+        const newStar = {
+          row: r,
+          column: c,
+          x: randomize(0, 100),
+          y: randomize(0, 100),
+        };
+        newStars.push(newStar);
+      }
+    }
+
+    setStars(newStars);
+  }, []);
 
   function handleResize() {
     if (ref.current) {
@@ -30,27 +52,27 @@ export default function Game() {
     }
   }
 
-  function createStars() {
-    let stars = [];
-    for (let r = 0; r < grid.rows; r++) {
-      for (let c = 0; c < grid.columns; c++) {
-        console.log(`star_row${r}_column${c}`);
-        stars.push(
-          <span
-            key={`star_row${r}_column${c}`}
-            className="star"
-            style={{
-              width: starDiameter,
-              height: starDiameter,
-              position: "absolute",
-              top: `${gridHeight / 2 + gridHeight * r - starDiameter / 2}px`,
-              left: `${gridWidth / 2 + gridWidth * c - starDiameter / 2}px`,
-            }}
-          ></span>
-        );
-      }
-    }
-    return stars;
+  function renderStars() {
+    const starNodes = stars.map((star) => (
+      <span
+        key={`star_row${star.row}_column${star.column}`}
+        className="star"
+        style={{
+          width: starDiameter,
+          height: starDiameter,
+          position: "absolute",
+          top: `${
+            ((gridHeight - starDiameter) / 100) * star.y + gridHeight * star.row
+          }px`,
+          left: `${
+            ((gridWidth - starDiameter) / 100) * star.x +
+            gridWidth * star.column
+          }px`,
+        }}
+      ></span>
+    ));
+
+    return starNodes;
   }
 
   return (
@@ -63,7 +85,7 @@ export default function Game() {
                 ref={ref}
                 style={{ width: "100%", height: `${screenHeight}px` }}
               >
-                {createStars()}
+                {renderStars()}
               </div>
             </Puffs>
           </Frame>
